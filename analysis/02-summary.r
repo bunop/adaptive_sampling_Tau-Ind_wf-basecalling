1a45f178e2254536919f4bef99bd5160c161a4e6
Paolo Cozzi <bunop@libero.it>
2025-07-15 14:33:47 +0200

    :monocle_face: load data from selected regions and make join

--- analysis/02-summary.r
+++ analysis/02-summary.r
@@ -8,9 +8,13 @@ library(here)
 sample_dir <- "output_methylong-5mC_5hmC/ont/A19_jun/pileup"
 
 # this is the path to the file
-sample_path <- here::here(
+methylation_path <- here::here(
   sample_dir,
   "A19_jun.bed.gz"
+)
+regions_path <- here::here(
+  "bed",
+  "ARS-UCD2.0_CpG-Islands_unix_buffer_merged.bed"
 )
 
 # attempt to save data to a file
@@ -19,7 +23,7 @@ con <- dbConnect(duckdb::duckdb(), dbdir = db_dir)
 
 # create the table if it does not exist
 if (!dbExistsTable(con, "methylation")) {
-  query_create <- sprintf(
+  create_methylation <- sprintf(
     "CREATE TABLE methylation AS
      SELECT
        \"#chrom\" AS chrom,
@@ -37,9 +41,41 @@ if (!dbExistsTable(con, "methylation")) {
        count_diff,
        count_nocall
      FROM read_csv_auto(%s, delim='\t', header=TRUE)",
-    dbQuoteString(con, sample_path)
+    dbQuoteString(con, methylation_path)
   )
-  dbExecute(con, query_create)
+  dbExecute(con, create_methylation)
+  cat("Table 'methylation' created successfully.\n")
+}
+
+# create a new table to load bed file
+if (!dbExistsTable(con, "regions")) {
+  create_regions <- sprintf(
+    "CREATE TABLE regions AS
+     SELECT
+       column0 AS chrom,
+       column1 AS chromStart,
+       column2 AS chromEnd,
+     FROM read_csv_auto(%s, delim='\t', header=FALSE)",
+    dbQuoteString(con, regions_path)
+  )
+  dbExecute(con, create_regions)
+  cat("Table 'regions' created successfully.\n")
+}
+
+# crate a table with methylation data in regions
+if (!dbExistsTable(con, "methylation_regions")) {
+  create_methylation_regions <- "
+    CREATE TABLE methylation_regions AS
+      SELECT
+        m.*
+      FROM methylation AS m
+      INNER JOIN regions AS r
+      ON m.chrom = r.chrom
+        AND m.chromStart >= r.chromStart
+        AND m.chromEnd < r.chromEnd
+  "
+  dbExecute(con, create_methylation_regions)
+  cat("Table 'methylation_regions' created successfully.\n")
 }
 
 # define a summary of a column
@@ -53,12 +89,12 @@ summary_valid_coverage <- dbGetQuery(con, "
     STDDEV(valid_coverage) AS sd_valid_coverage,
     QUANTILE_CONT(valid_coverage, 0.25) AS first_quartile,
     QUANTILE_CONT(valid_coverage, 0.75) AS third_quartile
-  FROM methylation
+  FROM methylation_regions
   USING SAMPLE 1 PERCENT (bernoulli)
 ")
 
 cat("valid_coverage\n")
-cat("   Min. :", summary_valid_coverage$min_valid_coverage, "\n")
+cat("   Min.   :", summary_valid_coverage$min_valid_coverage, "\n")
 cat("   1st Qu.:", summary_valid_coverage$first_quartile, "\n")
 cat("   Median :", summary_valid_coverage$median_valid_coverage, "\n")
 cat("   Mean   :", summary_valid_coverage$mean_valid_coverage, "\n")
