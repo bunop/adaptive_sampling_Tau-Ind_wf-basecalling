6296b5dd0935b29c02aeb1640bccdc894964e040
Paolo Cozzi <bunop@libero.it>
2025-08-08 13:35:55 +0200

    :sparkles: calculate coverage without filter

--- scripts/launch-bedMethyl2cov.sh
+++ scripts/launch-bedMethyl2cov.sh
@@ -4,8 +4,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --time=16:00:00
 #SBATCH --mem=16G
-#SBATCH --error=bedMethyl2cov.err
-#SBATCH --output=bedMethyl2cov.out
+#SBATCH --output=bedMethyl2cov.log
 #SBATCH --job-name=bedMethyl2cov
 #SBATCH --account=IscrC_BioGPUPX    # account name
 #SBATCH --partition=g100_usr_prod   # partition name (see https://docs.hpc.cineca.it/hpc/galileo.html#job-managing-and-slurm-partitions)
@@ -25,23 +24,33 @@ fi
 
 # Execute the bedMethyl2cov script with the specified input and output directories
 # CpG 5mCG and 5hmCG
-# scripts/bedMethyl2cov.py \
-#     -i output_methylong-5mCG_5hmCG-cpg/ont/ \
-#     -o output_methylong-5mCG_5hmCG-cpg/cov/ \
-#     &> 5mCG_5hmCG-cpg-cov.log
+if [ ! -d "output_methylong-5mCG_5hmCG-cpg/cov/" ]; then
+    scripts/bedMethyl2cov.py \
+        -i output_methylong-5mCG_5hmCG-cpg/ont/ \
+        -o output_methylong-5mCG_5hmCG-cpg/cov/ \
+        &> 5mCG_5hmCG-cpg-cov.log
+fi
 
-scripts/bedMethyl2cov.py \
-    -i output_methylong-5mCG_5hmCG-cpg/ont/ \
-    -o output_methylong-5mCG_5hmCG-cpg/cov_5X/ --custom_score 5 \
-    &> 5mCG_5hmCG-cpg-cov_5X.log
+if [ ! -d "output_methylong-5mCG_5hmCG-cpg/cov_5X/" ]; then
+    scripts/bedMethyl2cov.py \
+        -i output_methylong-5mCG_5hmCG-cpg/ont/ \
+        -o output_methylong-5mCG_5hmCG-cpg/cov_5X/ --custom_score 5 \
+        &> 5mCG_5hmCG-cpg-cov_5X.log
+fi
 
 # traditional 5mCG and 5hmCG
-# scripts/bedMethyl2cov.py \
-#     -i output_methylong-5mCG_5hmCG-traditional/ont/ \
-#     -o output_methylong-5mCG_5hmCG-traditional/cov/ \
-#     &> 5mCG_5hmCG-traditional-cov.log
+if [ ! -d "output_methylong-5mCG_5hmCG-traditional/cov/" ]; then
+    scripts/bedMethyl2cov.py \
+        -i output_methylong-5mCG_5hmCG-traditional/ont/ \
+        -o output_methylong-5mCG_5hmCG-traditional/cov/ \
+        &> 5mCG_5hmCG-traditional-cov.log
+fi
 
-scripts/bedMethyl2cov.py \
-    -i output_methylong-5mCG_5hmCG-traditional/ont/ \
-    -o output_methylong-5mCG_5hmCG-traditional/cov_5X/ --custom_score 5 \
-    &> 5mCG_5hmCG-traditional-cov_5X.log
+if [ ! -d "output_methylong-5mCG_5hmCG-traditional/cov_5X/" ]; then
+    scripts/bedMethyl2cov.py \
+        -i output_methylong-5mCG_5hmCG-traditional/ont/ \
+        -o output_methylong-5mCG_5hmCG-traditional/cov_5X/ --custom_score 5 \
+        &> 5mCG_5hmCG-traditional-cov_5X.log
+fi
+
+echo "bedMethyl2cov processing completed successfully."
